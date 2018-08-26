a9c4ea90ace431e5d8aa7e07d0df1b6881f41f82
praveen-me <praveenkrsaini0@gmail.com>
2018-08-27 01:36:14 +0530

    Add notification animation in day 14 project

--- projects/day_14_project/gulpfile.js
+++ projects/day_14_project/gulpfile.js
@@ -6,4 +6,7 @@ gulp.task('styles', function() {
     gulp.src('layout_1/style/style_dropdown.css')
     .pipe(autoprefixer())
     .pipe(gulp.dest('build'));
+    gulp.src('notification/style/notification.css')
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('build'));
 })
